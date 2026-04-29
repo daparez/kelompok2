46fa1c82616f49b93f7abef90a78f09fe7fadf97
daparez <dafarez03@gmail.com>
2026-04-29 09:49:44 +0700

    first commit

---
+++ konek.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "perpus02";
+
+$konek = mysqli_connect($host, $user, $pass, $db);
+
+if(!$konek){
+  echo  "konek gagal";
+}else {
+    echo "konek berhasil";
+}
+
+?>
